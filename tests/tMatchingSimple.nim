3657a180ddcaca91ea017696d0f076af09720e4b
haxscramper <haxscramper@gmail.com>
2020-10-23 15:20:15 +0300

    [>>>] Move matching to fusion

--- tests/tMatchingSimple.nim
+++
@@ -1,8 +0,0 @@
-import hmisc/macros/matching
-{.experimental: "caseStmtMacros".}
-
-if [1,2,3].matches [@head, all @tail]:
-  assert head == 1
-  assert tail == @[2,3]
-else:
-  raiseAssert("#[ IMPLEMENT ]#")
